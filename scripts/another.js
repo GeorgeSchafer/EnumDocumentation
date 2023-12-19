0daf02874943425927dc847f1da3596309ad468d
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2023-12-19 11:26:01 -0700

    first commit

---
+++ scripts/another.js
@@ -0,0 +1,9 @@
+
+/**
+ * @module another
+ */
+
+/**
+*  @description testing recursive file search.
+*  @summary This is the summary of the another.js file.*/
+let another = {};
---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+
+/**
+ * @module script
+ */
+
+/**
+ * @description Script.js performs stores elements, listeners and functions.
+ * This is the description of the script.js file. */
+let script = '';
+
+/** @summary code file that calls other *.js files. */
+function ear() {}
+
+/** @todo Finish the HTML document */
+const url = "";
+
+/** @todo fill the Elements object */
+const Elements = {};
+
+/** @todo fill the Listeners object */
+const Listeners = {};
+
+/** @todo fill the Functions object */
+const Functions = {};
